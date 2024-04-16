eac15ef4b9b9529b17088b2c8b04d4b608c641a5
Kenny Hou <kenny99hou@gmail.com>
2024-04-16 17:56:15 -0400

    my first commit

---
+++ cal_area.py
@@ -0,0 +1,7 @@
+def calculate_area(length, width):
+    """Calculates the area of a rectangle."""
+    return length * width
+
+area = calculate_area(5, 3)
+print(f"Area of rectangle: {area}")
+print (area)
