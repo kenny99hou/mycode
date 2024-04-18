671842d7257197b9c386d950c821df6638f6ad10
Kenny Hou <kenny99hou@gmail.com>
2024-04-18 17:13:33 -0400

    comit 2

---
+++ test_ai.py
@@ -0,0 +1,14 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_id = "mistral-community/Mixtral-8x22B-v0.1"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+model = AutoModelForCausalLM.from_pretrained(model_id)
+
+text = "Hello my name is"
+inputs = tokenizer(text, return_tensors="pt")
+
+outputs = model.generate(**inputs, max_new_tokens=20)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+print("test AI")
